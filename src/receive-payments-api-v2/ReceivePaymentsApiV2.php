2b7ef2b329528958e49d50438629229fd5578b95
a2881dante <a2881dante.dev@gmail.com>
2018-12-15 21:24:19 +0200

    Fix ssl.

--- src/receive-payments-api-v2/ReceivePaymentsApiV2.php
+++ src/receive-payments-api-v2/ReceivePaymentsApiV2.php
@@ -8,8 +8,15 @@ class ReceivePaymentsApiV2
     public static function getRecivingAdress($xpub, $apiKey, $callback){
         $root_url = 'https://api.blockchain.info/v2/receive';
         $parameters = 'xpub=' .$xpub. '&callback=' .urlencode($callback). '&key=' .$apiKey;
-        $response = file_get_contents($root_url . '?' . $parameters);
-        $object = json_decode($response);
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_URL => $root_url . '?' . $parameters,
+                CURLOPT_RETURNTRANSFER => true)
+        );
+        $data = curl_exec($ch);
+        curl_close($ch);
+        $object = json_decode($data);
         return $object;
     }
 
